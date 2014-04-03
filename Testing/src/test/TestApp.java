76e92ff8bc3f3883ec21253bf86271d404d10ad9
w0270525 <w0270525@nscc.ca>
2014-04-03 17:42:10 -0300

    gfbh

---
+++ Testing/src/test/TestApp.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello, this is but a test.");
+
+	}
+
+}
