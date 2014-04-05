cf6bd73b22c8279a3e0d79d8c4080d4197f32dd2
w0270525 <w0270525@nscc.ca>
2014-04-05 13:21:37 -0300

    v

--- GraphicsP213/src/graphics/FrameViewr.java
+++ GraphicsP213/src/graphics/FrameViewr.java
@@ -5,17 +5,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class FrameViewr {
-public static void main(String[] args)
-    {
-        JFrame frame = new JFrame();
-        frame.setSize(400, 400);
-        JLabel label = new JLabel("Hello, Jonathan!");
-        label.setOpaque(true);
-        label.setBackground(Color.green);
-        frame.add(label);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        
-    //vb was easier
-    }   
+    public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	frame.setSize(400, 400);
+	JLabel label = new JLabel("Hello, Jonathan!");
+	label.setOpaque(true);
+	label.setBackground(Color.green);
+	frame.add(label);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+
+	// vb was easier
+    }
 }
